f4edf9aa77a0547aec102e04e5ddfe775b3dfe48
Eddym07 <eddymartin0701@gmail.com>
2021-09-19 00:21:35 +0200

    résultat

--- js/chifoumi.js
+++ js/chifoumi.js
@@ -1,34 +1,44 @@
-//    var ciseaux = "ciseaux";
-//    var papier = "papier";
-//    var feuille = "feuille";
+
    
    //Acceuil du joueur.
     var firstName = prompt("bonjour ! Quel est ton prénom?");
-    alert("bienvenue sur CHI-FOU-MI " + firstName + "!  Jouons maintenant! Et que le meilleur gagne!");
+    alert("bienvenue sur CHIFOUMI " + firstName + "!  Jouons maintenant! Et que le meilleur gagne!");
+
+     //le joueur joue
 
     var choix = prompt("Choisis entre pierre, ciseaux ou papier");
     
+    // L'ordinateur joue
+
         alert("A mon tour de choisir maintenant!");
 
-    // var computer = [
-    //     "pierre",
-    //     "ciseaux",
-    //     "papier"
-    // ]
+    //fonction aléatoire pour faire choisir l'ordinateur
 
 function entierAleatoire(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var entier = entierAleatoire(1, 3);
+var computer = entierAleatoire(1, 3);
 
-if (entier === 1) {
-    alert("pierre");
+    //je determine quel objet l'ordinateur à choisi selon le chiffre aléatoire
+
+if (computer === 1) {
+    computer = ("pierre");
 }
-
-else if (entier === 2) {
-    alert("ciseaux");
+else if (computer === 2) {
+    computer = ("ciseaux");
 }
-
 else {
-    alert("papier");
+    computer = ("papier");
 } 
+    alert(computer);
+
+    
+if (computer == choix) {
+    alert("Match nul");
+}
+else if ((computer == "pierre" && choix == "papier") || (computer == "ciseaux" && choix == "pierre") || (computer == "papier" && choix == "ciseaux")) {
+    alert("gagné!");    
+}
+else {
+    alert("perdu");
+}
